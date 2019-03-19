5145042abb1861546425b0a1b228e9f538e682f0
Jason Graziano <mrehawk82@gmail.com>
2019-03-19 13:36:16 -0400

    getting oriented with UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,30 @@
-var myUI;
+var myUI, vGardener;
 
+vGardener = {
+    gBool = false
+};
 myUI =  {
 	init: () => {
-		var startBlock = "createEle() will be here soon!";
+		LSinit(vGardener);
+		myUI.loadout(vGardener);
+	},
+	loadout: (vGardener) => {
+		var startBlock = createEle("div"),
+		    newBtn = createEle("button");
+        
+        newBtn.innerHTML = "NEW GAME";
+        newBtn.className = "newBtn";
+        newBtn.onclick = myUI.newGameFunc(newBtn);
+
+		startBlock.className = "startBlock";
+        startBlock.append(newBtn, vGardener);
+
 		dvContain.append(startBlock);
+	},
+	newGameFunc: (newBtn, vGardener) => {
+		return () => {
+			alert(vGardener);
+		}
 	}
 };
 
